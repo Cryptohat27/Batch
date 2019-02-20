88ba4a0c68308c0cc36d651fee666559ec5f0ffe
Cryptohat27 <spencer.bischof1@gmail.com>
2019-02-20 16:10:04 +0530

    Update package install.bat

--- package install.bat
+++ package install.bat
@@ -1,19 +1,3 @@
-msiexec /i "C:\Windows\Temp\RemoteApp\RemoteApp\Leia RA.MSI" ALLUSERS=1 /q /norestart  /log c:\Leia.log
+msiexec /i "Hardpath\hardpath.msi" ALLUSERS=1 /q /norestart  /log c:\bla.log
 if not "%errorlevel%"=="0" exit /b %errorlevel%
-msiexec /i "C:\Windows\Temp\RemoteApp\RemoteApp\Public RA.MSI" ALLUSERS=1 /q /norestart  /log c:\public_ra.log
-if not "%errorlevel%"=="0" exit /b %errorlevel%
-msiexec /i "C:\Windows\Temp\RemoteApp\RemoteApp\Sage Desktop RA.MSI" ALLUSERS=1 /q /norestart  /log c:\sage_desktop.log
-if not "%errorlevel%"=="0" exit /b %errorlevel%
-msiexec /i "C:\Windows\Temp\RemoteApp\RemoteApp\TDX RA.MSI" ALLUSERS=1 /q /norestart  /log c:\tdx_ra.log
-if not "%errorlevel%"=="0" exit /b %errorlevel%
-msiexec /i "C:\Windows\Temp\RemoteApp\RemoteApp\TimberScan RA.MSI" ALLUSERS=1 /q /norestart  /log c:\timberscan_ra.log
-if not "%errorlevel%"=="0" exit /b %errorlevel%
-msiexec /i "C:\Windows\Temp\RemoteApp\RemoteApp\ENPRO Outside\DailySchedule.MSI" ALLUSERS=1 /q /norestart  /log c:\dailyschedule.log
-if not "%errorlevel%"=="0" exit /b %errorlevel%
-msiexec /i "C:\Windows\Temp\RemoteApp\RemoteApp\ENPRO Outside\ENPRO_PO.MSI" ALLUSERS=1 /q /norestart  /log c:\enpro_po.log
-if not "%errorlevel%"=="0" exit /b %errorlevel%
-msiexec /i "C:\Windows\Temp\RemoteApp\RemoteApp\ENPRO Outside\EnproJobOrdersNEW.MSI" ALLUSERS=1 /q /norestart  /log c:\enprojoborders.log
-if not "%errorlevel%"=="0" exit /b %errorlevel%
-msiexec /i "C:\Windows\Temp\RemoteApp\RemoteApp\ENPRO Outside\MasterPrice.MSI" ALLUSERS=1 /q /norestart  /log c:\MasterPrice.log
-if not "%errorlevel%"=="0" exit /b %errorlevel%
-msiexec /i "C:\Windows\Temp\RemoteApp\RemoteApp\ENPRO Outside\WhiteBoard.MSI" ALLUSERS=1 /q /norestart  /log c:\whiteboard.log
+msiexec /i "Hardpath\hardpath.MSI" ALLUSERS=1 /q /norestart  /log c:\public_bla.log
